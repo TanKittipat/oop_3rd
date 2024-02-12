58fe9fe6d4b80df4528fe5a67b02d0d7e18be8c9
TanKittipat <kittipat12tan@gmail.com>
2024-02-12 16:59:28 +0700

    2nd

---
+++ Ex3.js
@@ -0,0 +1,32 @@
+class Person {
+  name = "";
+  address = "";
+  constructor(name, address) {
+    this.name = name;
+    this.address = address;
+  }
+}
+
+class Student {
+  name = "";
+  address = "";
+  program = "";
+  year = 0;
+  fee = 0;
+  constructor(program, year, fee) {
+    this.program = program;
+    this.year = year;
+    this.fee = fee;
+  }
+}
+
+class Staff {
+  name = "";
+  address = "";
+  school = "";
+  pay = 0;
+  constructor(school, pay) {
+    this.school = school;
+    this.pay = pay;
+  }
+}
